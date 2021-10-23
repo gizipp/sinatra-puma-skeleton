859c5718935ecdafe0a878d59f2f8b69abe8e53e
Gilang R. Aprianto <gizipp.gizipp@gmail.com>
2021-10-23 22:59:34 +0700

    resolved

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'mechanize'
 require 'json'
-
+require 'faraday'
 
 before do
   content_type :json
@@ -16,9 +16,16 @@ post '/check' do
   page  = agent.get(params[:url])
   links = []
 
-  page.links.compact.each do |link|
-    puts link.href
-    links << link.href
+  page.links.each do |link|
+    url = link.resolved_uri rescue nil
+    puts "checking #{url}"
+    res = Faraday.get(url) if url
+
+    links << {
+      anchor: link.text,
+      url: url,
+      status: res.status
+    } unless url.nil?
   end
 
   { links: links }.to_json
