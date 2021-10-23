274f0f48ad0407067df276fd3504df3f0633d584
Gilang R. Aprianto <gizipp.gizipp@gmail.com>
2021-10-23 22:43:21 +0700

    scrape

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'mechanize'
+require 'json'
+
+
+before do
+  content_type :json
+end
 
 get '/' do
   'Hello world'
+end
+
+post '/check' do
+  agent = Mechanize.new
+  page  = agent.get(params[:url])
+  links = []
+
+  page.links.compact.each do |link|
+    puts link.href
+    links << link.href
+  end
+
+  { links: links }.to_json
 end
